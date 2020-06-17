951d30c77066b91d410e55e70f9f46bb06b3646d
pavan <dongare.pavan25@gmail.com>
2020-06-18 03:38:05 +0530

    ckpt

--- client/src/components/auth/Regsiter.js
+++ client/src/components/auth/Regsiter.js
@@ -1,11 +1,106 @@
-import React from 'react'
+import React, {Fragment, useState } from 'react'
+import { Link } from 'react-router-dom';
 
 export const Regsiter = () => {
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: ''
+    });
+    /*
+     * why use useState hook?
+     *   1 we can eaisly access formData directly
+     *   2 set using setForm method defined
+     */
+
+    const { name,email,password,password2} = formData;
+    
+    /*
+        setformData
+        param: 1 single object { original obj , data to change in original }
+        original: ...formData // copy or main state
+        data to change: [e.target.name]: e.target.value
+                        [e.target.name] -> gets name of attribute of html element
+                        e.target.value  -> value
+
+        question?
+        why [] syntax ?
+        why : syntax ?
+    */
+
+        /* 
+            value="name" // sets value to 'name' literally
+            value={name} // value of variable name
+            Question? is it two way binding or one way?
+            one way with {} // html to js
+        */
+    const onChange = e=> setFormData({ ...formData , [e.target.name]: e.target.value });
+
+    const onSubmit = e => {
+        e.preventDefault();
+        if(password !== password2 )
+            console.log('passwords do not match');
+        else 
+            console.log(formData);   
+    }
+
     return (
-        <div>
-            Register
-        </div>
-    )
+       <Fragment>
+        <h1 className="large text-primary">Sign Up</h1>
+        <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+        <form className="form" onSubmit={e=>onSubmit(e)}>
+          <div className="form-group">
+            <input type="text" 
+                   placeholder="Name" 
+                   name="name" 
+                   value={name}
+                   onChange = { e=> onChange(e) }
+                   required />
+  
+        
+          </div>
+          <div className="form-group">
+            <input type="email" 
+                   placeholder="Email Address" 
+                   name="email" 
+                   value = {email}
+                   onChange = { e=> onChange(e) }
+                   
+                   />
+            <small className="form-text"
+              >This site uses Gravatar so if you want a profile image, use a
+              Gravatar email</small
+            >
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              minLength="6"
+              value = {password}
+              onChange = { e=> onChange(e) }
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              name="password2"
+              minLength="6"
+              value = {password2}
+              onChange = { e=> onChange(e) }
+            />
+          </div>
+          <input type="submit" className="btn btn-primary" value="Register" />
+        </form>
+        <p className="my-1">
+          Already have an account? <Link to="/login">Sign In</Link>
+        </p>
+      </Fragment>
+    );
 }
 
 
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,17 +5,17 @@ export const Navbar = () => {
   return (
       <nav className="navbar bg-dark">
         <h1>
-          <a href="index.html">
+          <Link to="">
             <i className="fas fa-code"></i> DevConnector
-          </a>
+          </Link>
         </h1>
         <ul>
-          <li> <Link to="!#">Developers</Link></li>
+          <li> <Link to="">Developers</Link></li>
           <li> <Link to="/register">Register</Link>  </li>
           <li> <Link to="/login">Login</Link>        </li>
         </ul>
       </nav>
   );
 };
-
+ 
 export default Navbar
