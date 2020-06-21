97e9bc47564c2c7c1362506de175ee87238ccdea
pavan <dongare.pavan25@gmail.com>
2020-06-21 08:17:15 +0530

    dashboard component

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,19 @@ import React, { Fragment , useEffect,cleanup } from 'react';
 import './App.css';
 
 import { Landing } from './components/layout/Landing';
-import  Alert  from './components/layout/Alert';
-import  Navbar  from './components/layout/Navbar'; 
+
 import { BrowserRouter as Router , Route , Switch } from 'react-router-dom';
-import Regsiter from './components/auth/Regsiter';
-import Login from './components/auth/Login';
+
 import { Provider } from 'react-redux';
 import store from './store';
 import setAuthToken from './utils/setAuthToken';
 import { loadUser } from './actions/authAction';
+// components
+import Regsiter from './components/auth/Regsiter';
+import Login from './components/auth/Login';
+import  Alert  from './components/layout/Alert';
+import  Navbar  from './components/layout/Navbar'; 
+import  Dashboard     from './components/dashboard/Dashboard';
 // prefer default import, named import on navbar gives an error
 /*
   mistake: 2 exports were named same, default & named
@@ -40,6 +44,7 @@ const App = ()=> {
                       <Switch>          
                           <Route path="/register" component={Regsiter} />
                           <Route path="/login" component={Login} />
+                          <Route path="/dashboard" component={Dashboard}/>
                       </Switch>
           
                   </section>   
