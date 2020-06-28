e175d5e86403cd92607dc8ff72026636c01ee8c3
pavan <dongare.pavan25@gmail.com>
2020-06-28 21:58:28 +0530

    ws

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -1,16 +1,54 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
-const ProfileItem = props => {
+const ProfileItem = ({profile :{
+    profile_id,
+    email,
+    profileData
+}}) => {
+    const [localState, setProfileState]= useState({
+        company:'',
+        website:'',
+        location:'',
+        status:'',
+        skills:'',
+        githubusername:'',
+        bio:'',
+        twitter:'',
+        facebook:'',
+        linkedin:'',
+        youtube:'',
+        instagram:''
+    });
+    const  {
+        company,
+        website,
+        location,
+        status,
+        skills,
+        githubusername,
+        bio,
+        twitter,
+        facebook,
+        linkedin,
+        youtube,
+        instagram, 
+    } = localState;
+    //setProfileState({...localState,profileData});
     return (
-        <div>
-            
+        <div className="profile bg-light">
+            <img  className="round-img"/>
+            <div>
+                <h2>{email}</h2>
+                <h2>{company}</h2>
+            </div>
         </div>
     )
 }
 
 ProfileItem.propTypes = {
-
+  profile: PropTypes.object.isRequired,
 }
 
 export default ProfileItem
--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -1,18 +1,37 @@
-import React , { Fragment, useEffect } from 'react'
+import React , { Fragment, useEffect,useState } from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import Spinner from '../layout/spinner'
 import { getAllProfiles  }  from '../../actions/profile'
+import { LOGIN_FAIL } from '../../actions/types'
+import  ProfileItem  from './ProfileItem'
 
 
-const Profiles = ({getAllProfiles, profile:{ profiles,loading }}) => {
+const Profiles = ({getAllProfiles, profile:{ profiles,loading,profileData }}) => {
+   
+
     useEffect(()=>{
-        getAllProfiles()
+        getAllProfiles();
+        console.log(profileData);
+        //setProfileData(JSON.parse(profileData));
     },[]);
     return (
-        <div>
-            test
-        </div>
+        <Fragment>
+            {
+                loading ? <Spinner/> :
+                <Fragment>
+                    <h1 className='large text-primary' > Developers </h1>  
+                    <p className='lead' >
+                        <i className="fab fa-connectdevelop"></i> Browse & connect
+                    </p>
+                    <div className="profiles">
+                        { profiles.length >0 ? (
+                            profiles.map(profile=>(<ProfileItem key={profile.profile_id} profile={profile} />))
+                        ) : <h4> No profiles found </h4>}
+                    </div>
+                </Fragment>
+            }
+        </Fragment>
     )
 }
 
